79cb830ea2add3929808132bde7a0e0ba0176b31
rahulbansal <rahulmay94@gmail.com>
2023-05-16 11:38:55 +0530

    Now generator function is a thread and signals the parent process with SIGUSR1 on its completion

--- datastream.c
+++ datastream.c
@@ -1,49 +1,84 @@
 #include <stdio.h> // printf() fopen() fclose()
 #include <stdlib.h> // atoi() rand()
 #include <string.h> // strcmp()
+#include <pthread.h> // pthread_create() pthread_t
+#include <signal.h> // signal() kill()
+#include <unistd.h> // getpid()
 
-int NUMBER_OF_KEYS = 1000; // 1000 KEYS
-int KEY_SIZE = 1000; // 1000 CHAR 
-int BUFFER_SIZE = 10; // 10 KEYS
-int DELAY_PRODUCER = 0; // 0 NANO SEC
-int DELAY_CONSUMER = 0; // 0 NANO SEC
+int number_of_keys = 1000; // 1000 KEYS
+int key_size = 1000; // 1000 CHAR 
+int buffer_size = 10; // 10 KEYS
+int delay_producer = 0; // 0 NANO SEC
+int delay_consumer = 0; // 0 NANO SEC
+int pid = 0;
 
-char CHARSET[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+char CHARSET[] = 
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
+void sigusr1_handler(int num);
+void sigint_handler(int num);
 void random_string(char str[], unsigned int str_size);
-void generator();
+void* generator(void* arg);
 
 int main(int argc, char* argv[])
 {
-    // Command Line Arguments
-    for(int i = 1; i < argc; i++) 
+    pid = getpid();
+    
+    for(int i = 1; i < argc; i++) // Command Line Arguments
     {
         if(strcmp(argv[i], "--number_of_keys"))
         {
-            NUMBER_OF_KEYS = atoi(argv[++i]);
+            number_of_keys = atoi(argv[++i]);
         }
         else if(strcmp(argv[i], "--key_size"))
         {
-            KEY_SIZE = atoi(argv[++i]);
+            key_size = atoi(argv[++i]);
         }
         else if(strcmp(argv[i], "--buffer_size"))
         {
-            BUFFER_SIZE = atoi(argv[++i]);
+            buffer_size = atoi(argv[++i]);
         }
         else if(strcmp(argv[i], "--delay_producer"))
         {
-            DELAY_PRODUCER = atoi(argv[++i]);
+            delay_producer = atoi(argv[++i]);
         }
         else if(strcmp(argv[i], "--delay_consumer"))
         {
-            DELAY_CONSUMER = atoi(argv[++i]);
+            delay_consumer = atoi(argv[++i]);
         }
     }
 
-    generator();
+    printf("Setting Handlers\n");
+    signal(SIGUSR1, sigusr1_handler);
+    signal(SIGINT, sigint_handler);
+
+    printf("Creating generator thread\n");
+
+    pthread_t generator_thread_id;
+    int generator_thread_arg = 0;
+
+    int generator_pthread_create_return_value = 
+        pthread_create(&generator_thread_id, NULL,  generator, &generator_thread_arg);
+
+    printf("%d\n", generator_pthread_create_return_value);
+
+    int generator_pthread_join_return_value = 
+        pthread_join(generator_thread_id, NULL);
+
+    printf("%d\n", generator_pthread_join_return_value);
 
     return 0;
     
+}
+
+void sigusr1_handler(int num)
+{
+    printf("sigusr1_handler\n");
+}
+
+void sigint_handler(int num)
+{
+    printf("sigint_handler\n");
 }
 
 void random_string(char str[], unsigned int str_size)
@@ -58,16 +93,20 @@ void random_string(char str[], unsigned int str_size)
     return;
 }
 
-void generator()
+void* generator(void* arg)
 {
+    printf("generator\n");
+
     FILE* fptr;
     fptr = fopen("original.txt", "w");
 
-    char str[KEY_SIZE];
-    for(int i = 0; i < NUMBER_OF_KEYS; i++)
+    char str[key_size];
+    for(int i = 0; i < number_of_keys; i++)
     {
-        random_string(str, KEY_SIZE);
+        random_string(str, key_size);
         fprintf(fptr, "%s", str);
     }
     fclose(fptr);
+    kill(pid, SIGUSR1);
+    return NULL;
 }
