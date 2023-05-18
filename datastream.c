62a34d3aba62d8840ed61cf705cf618a11100b59
rahulbansal <rahulmay94@gmail.com>
2023-05-18 22:34:52 +0530

    added some logs

--- datastream.c
+++ datastream.c
@@ -1,21 +1,18 @@
-#include <stdio.h> // printf() fopen() fclose() fprintf()
-#include <stdlib.h> // atoi() rand()
-#include <string.h> // strcmp()
-#include <pthread.h> // pthread_create() pthread_t
-#include <signal.h> // signal() kill()
-#include <unistd.h> // getpid() pid_t fork() getcwd()
-#include <time.h> // time_t
-#include <sys/ipc.h> // ftok()
-#include <sys/shm.h> // shmget()
-#include <errno.h> // 
+#include <stdio.h>    // printf() fopen() fclose() fgets() fputs() fprintf()
+#include <stdlib.h>   // atoi() rand()
+#include <string.h>   // strcmp()
+#include <pthread.h>  // pthread_create() pthread_t
+#include <signal.h>   // signal() kill()
+#include <unistd.h>   // getpid() pid_t fork() getcwd() sleep() getppid()
+#include <time.h>     // time_t
+#include <sys/ipc.h>  // ftok()
+#include <sys/shm.h>  // shmget()
 
-int number_of_keys = 10;//KEYS
-int buffer_size = 3;//KEYS
+int number_of_keys = 10; // KEYS
+int buffer_size = 3;     // KEYS
 
 pid_t producer_pid;
 pid_t consumer_pid;
-
-pid_t pid = 0;//PROCESS ID
 
 // charset is used in choosing a random char
 char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -30,11 +27,9 @@ void create_buffer();
 void producer();
 void consumer();
 
+
 int main(int argc, char* argv[])
-{
-    printf("**** Program Start ****\n");
-    pid = getpid();
-    
+{   
     // Command Line Arguments
     for(int i = 1; i < argc; i++)
     {
@@ -64,6 +59,7 @@ int main(int argc, char* argv[])
     
 }
 
+
 struct Item
 {
     char data[11];
@@ -75,25 +71,21 @@ struct Item
 
 void sigusr1_handler(int num)
 {
-    printf("inside sigusr1\n");
     create_buffer();
 
     producer_pid = fork();
     if(producer_pid == 0)
     {
-        printf("producer fork\n");
         producer();
         exit(0);
     }
-    printf("producer pid : %d\n", producer_pid);
+    
     consumer_pid = fork();
     if(consumer_pid == 0)
     {
-        printf("consumer fork\n");
         consumer();
         exit(0);
     }
-    printf("consumer pid : %d\n", consumer_pid);
 }
 
 void random_string(char str[], unsigned int str_size)
@@ -105,9 +97,9 @@ void random_string(char str[], unsigned int str_size)
     return;
 }
 
+
 void* generator(void* arg)
 {
-    printf("inside gen\n");
     FILE* fptr;
     fptr = fopen("original.txt", "w");
 
@@ -120,40 +112,37 @@ void* generator(void* arg)
 
     fclose(fptr);
 
-    kill(pid, SIGUSR1);
+    kill(getpid(), SIGUSR1);
 }
+
 
 void buffer_path(char *cwd, int cwd_size)
 {
     getcwd(cwd, cwd_size);
     char str[] = "/buffer";
+
     int i = 0;
-    while(cwd[i] != '\0') 
-    {
-        i++;
-    }
+
+    while(cwd[i] != '\0') { i++; }
+
     for(int j = 0; j < sizeof(str); j++)
     {
         cwd[i] = str[j];
         i++;
     }
+
     cwd[i] = '\0';
 }
 
+
 void create_buffer()
 {
-    printf("inside create_buffer\n");
     char cwd[4096];
     buffer_path(cwd, 4096);
 
-    key_t key_a = ftok(cwd, getpid());
+    key_t key_a = ftok(cwd, 'a');
     int shm_id_a = shmget(key_a, sizeof(struct Item) * buffer_size, 0666|IPC_CREAT);
     struct Item *buffer = (struct Item*)shmat(shm_id_a, 0, 0);
-
-    printf("key_a : %d\n", key_a);
-    printf("shm_id_a : %d\n", shm_id_a);
-
-    printf("errno %d\n", errno);
 
     for(int i = 0; i < buffer_size; i++) 
     {
@@ -161,18 +150,15 @@ void create_buffer()
     }
 }
 
+
 void producer()
 {
-    printf("inside producer\n");
     char cwd[4096];
     buffer_path(cwd, 4096);
 
-    key_t key_a = ftok(cwd, getppid());
+    key_t key_a = ftok(cwd, 'a');
     int shm_id_a = shmget(key_a, sizeof(struct Item) * buffer_size, 0);
     struct Item *buffer = (struct Item*)shmat(shm_id_a, 0, 0);
-    
-    printf("producer key_a : %d\n", key_a);
-    printf("producer shm_id_a : %d\n", shm_id_a);
 
     FILE* fptr;
     fptr = fopen("original.txt", "r");
@@ -181,33 +167,32 @@ void producer()
 
     while(feof(fptr) == 0) 
     {
-        printf("producer loop %d\n", i);
-        while(buffer[i].write == 0) {}
+        while(buffer[i].write == 0) {}  // busy-waiting
+
         fgets(buffer[i].data, 11, fptr);
-        printf("key : %s\n", buffer[i].data);
         buffer[i].pid = getpid();
         time(&buffer[i].creation_time);
         buffer[i].write = 0;
+
+        // log
+        printf("++producer:  key: %s  time: %li\n", buffer[i].data, buffer[i].creation_time);
+
         i++;
         i = i % buffer_size;
     }
     
     fclose(fptr);
-    printf("producer exit\n");
 }
+
 
 void consumer()
 {
-    printf("inside consumer\n");
     char cwd[4096];
     buffer_path(cwd, 4096);
 
-    key_t key_a = ftok(cwd, getppid());
+    key_t key_a = ftok(cwd, 'a');
     int shm_id_a = shmget(key_a, sizeof(struct Item) * buffer_size, 0);
     struct Item *buffer = (struct Item*)shmat(shm_id_a, 0, 0);
-
-    printf("consumer key_a : %d\n", key_a);
-    printf("consumer shm_id_a : %d\n", shm_id_a);
 
     FILE* fptr;
     fptr = fopen("duplicate.txt", "w");
@@ -217,16 +202,20 @@ void consumer()
 
     while(count < number_of_keys) 
     {
-        printf("consumer loop %d\n", i);
-        while(buffer[i].write == 1) {}
-        fprintf(fptr, buffer[i].data);
+        while(buffer[i].write == 1) {}  // busy-waiting
+
+        fputs(buffer[i].data, fptr);
         buffer[i].write = 1;
-        i++;
+
+        // log
+        printf("--consumer:  key: %s  time: %li\n", buffer[i].data, time(0));
+
         count++;
+        i++;
         i = i % buffer_size;
     }
 
     fclose(fptr);
-    printf("consumer exit\n");
+
     kill(getppid(), SIGINT);
 }
